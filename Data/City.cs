b79cf3657f9b9a94111448bf1a6d1ed552af3e11
Zeeshan <zk55397@gmail.com>
2026-02-05 12:47:59 +0530

    change in table added styling

--- Data/City.cs
+++ Data/City.cs
@@ -5,7 +5,7 @@ namespace WebTestMVC.Data
     public class City
     {
         [Key]public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public int StateId {  get; set; }
     }
 }
